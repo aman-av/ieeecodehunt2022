350b186b2957cf63ab7072a9956eec3de4173a4e
Prakhar Awasthi <pawasthi961@gmail.com>
2021-09-05 20:50:48 +0530

    latest commit

--- client/src/App.js
+++ client/src/App.js
@@ -10,8 +10,8 @@ import Example from "./example";
 import Test from "./Test";
 
 function App() {
-  const eventEndDate =  new Date( 2021,8,9,22,42,0,0);
-  const eventStartDate = new Date(2021, 8, 9, 22, 40, 0, 0);
+  const eventEndDate =  new Date(2021,9,11,22,42,0,0);
+  const eventStartDate = new Date(2021, 9, 5, 20, 40, 0, 0);
   return (
     <div>
       <Header />
