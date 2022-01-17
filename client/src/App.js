127ea0174fa9dc9ea4e12d8ba011edc827252e71
Prakhar Awasthi <pawasthi961@gmail.com>
2022-01-17 21:59:01 +0530

    testing1

--- client/src/App.js
+++ client/src/App.js
@@ -10,8 +10,8 @@ import Example from "./example";
 import Test from "./Test";
 
 function App() {
-  const eventStartDate = new Date(2022,1,17,15,54,0);
-  const eventEndDate =  new Date(2022,1,17,19,30,0);
+  const eventStartDate = new Date(2022,1,17,22,4,0);
+  const eventEndDate =  new Date(2022,1,17,23,59,0);
   
 
   return (
